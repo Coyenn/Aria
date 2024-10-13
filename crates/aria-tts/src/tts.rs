966378b408048ca517a447a8769547331bdba553
Tim Ritter <timrittermail@gmail.com>
2024-10-13 12:25:21 +0200

    Make tts work

--- crates/aria-tts/src/tts.rs
+++ crates/aria-tts/src/tts.rs
@@ -1,53 +1,33 @@
-use std::sync::Arc;
-use tokio::sync::Mutex;
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
 use windows::{
     core::HSTRING,
     Media::{Core::MediaSource, Playback::MediaPlayer, SpeechSynthesis::SpeechSynthesizer},
 };
 
-struct SpeechState {
-    player: MediaPlayer,
-    synthesizer: SpeechSynthesizer,
-}
+static PLAYER: Lazy<Mutex<MediaPlayer>> = Lazy::new(|| Mutex::new(MediaPlayer::new().unwrap()));
 
-lazy_static::lazy_static! {
-    static ref SPEECH_STATE: Arc<Mutex<Option<SpeechState>>> = Arc::new(Mutex::new(None));
-}
-
-pub async fn say(text: &str) -> windows::core::Result<()> {
+pub fn say(text: &str) -> windows::core::Result<()> {
     log::info!("Saying: {}", text);
 
-    let mut state = SPEECH_STATE.lock().await;
+    let synthesizer = SpeechSynthesizer::new()?;
+    let text = text.to_string();
 
-    // Cancel the previous playback if it exists
-    if let Some(prev_state) = state.as_ref() {
-        prev_state.player.Pause()?;
-    }
-
-    // Create a new SpeechState if it doesn't exist
-    if state.is_none() {
-        *state = Some(SpeechState {
-            player: MediaPlayer::new()?,
-            synthesizer: SpeechSynthesizer::new()?,
-        });
-    }
-
-    let speech_state = state.as_ref().unwrap();
-
-    // Synthesize speech
-    let stream = speech_state
-        .synthesizer
-        .SynthesizeTextToStreamAsync(&HSTRING::from(text))
-        .unwrap()
-        .get()
-        .unwrap();
-
+    let stream = synthesizer
+        .SynthesizeTextToStreamAsync(&HSTRING::from(text))?
+        .get()?;
     let media_source =
         MediaSource::CreateFromStream(&stream, &HSTRING::from(stream.ContentType()?))?;
 
-    // Set the source and play
-    speech_state.player.SetSource(&media_source)?;
-    speech_state.player.Play()?;
+    // Get the locked player instance
+    let player = PLAYER.lock().unwrap();
+
+    // Stop any currently playing audio
+    player.Pause()?;
+
+    // Set the new source and play
+    player.SetSource(&media_source)?;
+    player.Play()?;
 
     Ok(())
 }
