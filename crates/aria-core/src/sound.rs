a1339eccbd6b5f4146ee88728b975e3d7182d4f4
Tim Ritter <timrittermail@gmail.com>
2024-11-21 00:30:08 +0100

    Small fix cause of blog post

--- crates/aria-core/src/sound.rs
+++ crates/aria-core/src/sound.rs
@@ -5,22 +5,19 @@ pub const STARTUP_SOUND: &[u8] = include_bytes!("../assets/sounds/startup.mp3");
 pub const SHUTDOWN_SOUND: &[u8] = include_bytes!("../assets/sounds/shutdown.mp3");
 pub const INPUT_FOCUSSED_SOUND: &[u8] = include_bytes!("../assets/sounds/input-focussed.mp3");
 
-pub fn play_sound(sound: &[u8]) {
-    let sound_copy = sound.to_vec();
+/// Plays the given sound data asynchronously on the default audio output.
+pub fn play_sound(sound_data: &[u8]) {
+    let sound_data_clone = sound_data.to_vec();
 
     std::thread::spawn(move || {
-        // Play using rodio
         let (_stream, stream_handle) = OutputStream::try_default().unwrap();
         let sink = Sink::try_new(&stream_handle).unwrap();
 
-        // Add a dummy source of the sake of the example.
-        let cursor = Cursor::new(sound_copy);
+        let cursor = Cursor::new(sound_data_clone);
         let source = Decoder::new(cursor).unwrap();
 
         sink.append(source);
 
-        // The sound plays in a separate thread. This call will block the current thread until the sink
-        // has finished playing all its queued sounds.
         sink.sleep_until_end();
     });
 }
