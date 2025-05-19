c50263281407c13979ffe839d5543490d5070809
Tim Ritter <timrittermail@gmail.com>
2025-05-19 22:10:03 +0200

    Switch to flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Aria development flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    rustupToolchain = "stable";
+    rustBuildTargetTriple = "x86_64-pc-windows-gnu";
+    rustBuildHostTriple = "x86_64-unknown-linux-gnu";
+
+    # Cross-compilation package set for Windows target
+    pkgsCross = import nixpkgs {
+      inherit system;
+      crossSystem = {config = "x86_64-w64-mingw32";};
+    };
+
+    mingw_w64_cc = pkgsCross.stdenv.cc;
+    mingw_w64 = pkgsCross.windows.mingw_w64;
+    mingw_w64_pthreads_w_static = pkgsCross.windows.mingw_w64_pthreads.overrideAttrs (oldAttrs: {
+      configureFlags = (oldAttrs.configureFlags or []) ++ ["--enable-static"];
+    });
+  in {
+    devShells.${system}.default = pkgs.mkShell rec {
+      buildInputs = with pkgs; [rustup mingw_w64_cc yq];
+
+      # Default Rust toolchain and target
+      RUSTUP_TOOLCHAIN = rustupToolchain;
+      CARGO_BUILD_TARGET = rustBuildTargetTriple;
+
+      shellHook = ''
+        # Ensure our Windows target is added and rustfmt is available
+        rustup target add "${rustBuildTargetTriple}"
+        rustup component add rustfmt
+      '';
+
+      # Pass linker flags for static pthread support
+      RUSTFLAGS = builtins.map (a: ''-L ${a}/lib'') [mingw_w64 mingw_w64_pthreads_w_static];
+    };
+  };
+}
